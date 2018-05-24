48708ea33eba8f8bb300b5ff75e2c39daefa81c1
xiaopc <jeiker@126.com>
2018-05-24 22:43:17 +0800

    init project
    init project

---
+++ security-demo2/src/main/java/com/jeiker/SecurityDemo2Application.java
@@ -0,0 +1,12 @@
+package com.jeiker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemo2Application.class, args);
+	}
+}
