420604d6a650ef96fe6af0a59f7801cd35f762b2
jeikerxiao <jeiker@126.com>
2018-06-02 16:02:15 +0800

    add demo3

---
+++ security-demo3/src/main/java/com/jeiker/SecurityDemo3Application.java
@@ -0,0 +1,12 @@
+package com.jeiker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemo3Application.class, args);
+    }
+}
